c77f485aa1529cf22505438b33ab13e0b57c7cac
yuinore <yuinore@hotmail.co.jp>
2022-02-10 07:29:25 +0900

    グラフ表示を実装

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,7 +1,32 @@
 import * as React from 'react';
+import { Chart } from 'react-google-charts';
+
+export const data = [
+  ['X-axis', 'series 1', { role: 'style' }, 'series 2', { role: 'style' }],
+  ['day 1', 10, '#ff8888', 30, '#ffcc66'],
+  ['day 2', 20, '#ff8888', 40, '#ffcc66'],
+];
+
+export const options = {
+  chart: {
+    title: 'Stacked Bar Chart',
+    subtitle: 'Powered by React Google Charts',
+  },
+  isStacked: true,
+
+  colors: ['#ff8888', '#ffcc66'],
+};
 
 function Hello(): JSX.Element {
-  return <h1>Hello!</h1>;
+  return (
+    <Chart
+      chartType='ColumnChart'
+      data={data}
+      width='100%'
+      height='400px'
+      options={options}
+    />
+  );
 }
 
 export default Hello;
